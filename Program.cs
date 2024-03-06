342b4d7051765bb119c5c3b783f2b7cb47c5af37
mertc <mertc@DESKTOP-8ID65LJ>
2024-03-06 15:33:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("Bir pozitif tam sayi giriniz:");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+int bolenlerinToplami = 0;
+
+for(int i=1; i<=sayi/2; i++)  //yarisina kadar bakiyoruz cunku yarisindan buyuk tam boleni olamaz.
+{
+    if (sayi%i == 0)   //tam boleni kontrol
+    {
+        bolenlerinToplami += i;  //tam boleni bolenlerinToplamina ekleme
+    }
+}
+
+if (bolenlerinToplami == sayi) 
+{
+    Console.WriteLine(sayi + " sayisi bir PerfectNumber dir.");
+}
+else
+{
+    Console.WriteLine(sayi + " sayisi bir PerfectNumber degildir.");
+}
+
